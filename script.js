c1719334cedcc7d7a036b6172027a22671e3717f
Jank Jank <jank.jank1812@gmail.com>
2021-05-16 22:39:06 -0700

    beginning conditionals for inputs

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialCharacters = [
-  "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
+var specialCharacters = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
   "~", "*", "?", "\"", "\\", ":", ];
 var lowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
 var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"]
 
 
+//Asking for user input Length
+var userLength = window.prompt("How many letters would you like in your password? Please choose one in the range of 8-128");
 
-var arrayLength = { prompt("How many letters would you like in your password?");
+  if(isNaN(userLength)){
+    document.write(num1 + " is not a number <br/>");
+ }
+  if (userLength <8) {return;
 }
+  if (userLength > 128) {return;
+}
+
+
+// Asking for special character types
+let userSpecialCharacters = true;
+confirm("Would you like to use numbers in your password?");
+let userSpecialCharacters = true;
+confirm("lowercase letters?");
+let userSpecialCharacters = true;
+confirm("UPPERCASE LETTERS?");
+let userSpecialCharacters = true;
+confirm("!&#@  Special Characters?");
+
+
+
 
 
 
