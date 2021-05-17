cf1efcea02bb9c0df5770e778ca2f8c81d6078e2
Jank Jank <jank.jank1812@gmail.com>
2021-05-16 23:24:51 -0700

    adding more comments on layout

--- script.js
+++ script.js
@@ -21,23 +21,36 @@ var userLength = window.prompt("How many letters would you like in your password
 
 
 // Asking for special character types
-let userSpecialCharacters = true;
+let userNumeric = true;
 confirm("Would you like to use numbers in your password?");
-let userSpecialCharacters = true;
+let userSpecial = true;
 confirm("lowercase letters?");
-let userSpecialCharacters = true;
+let userLowerCase = true;
 confirm("UPPERCASE LETTERS?");
-let userSpecialCharacters = true;
+let userUpperCase = true;
 confirm("!&#@  Special Characters?");
 
+// if all the character confirms are false, tell user they must 
+// pick at least one group and return to stop the process, start over.  
 
 
+//function to generate password
+  //variable=password options function
+  //array=[]
+  //check if options exist, return
+  //check if options have special characters
+  //array=array.concat(specialCharacters)
+  //check if options have special characters
+  //array=array.concat(lowerCaseCharacters)
+  //check if options have special characters
+  //array=array.concat(upperCaseCharacters)
+  //check if options have special characters
+  //array=array.concat(numericCharacters)
+  //for loop[ to call randomlygenerater password, pass our array]
 
-
-
-
-
-
+//funtionto rtandomly generate password (array)
+//var options = Math.floor(Math.random() * array.length)
+//return options
 
 
 // Write password to the #password input
